bf1ed574aabf793fefa85c0cdc1b3c34d6ffa45c
Danil Alexeev <danil@alexeev.xyz>
2021-04-16 14:49:32 +0300

    Исправлено форматирование в index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,14 +2,14 @@
 
 // == Подготовка ядра ==
 
-chdir(__DIR__.'/..');
+chdir(__DIR__ . '/..');
 
 require 'core/main.php';
 
 // == Передача управления контроллеру ==
 
-foreach(require 'config/routes.php' as $pattern => $controller) {
-	if(preg_match($pattern, REQUEST_PATH)) {
+foreach (require 'config/routes.php' as $pattern => $controller) {
+	if (preg_match($pattern, REQUEST_PATH)) {
 		require "controllers/$controller.php";
 		break;
 	}
